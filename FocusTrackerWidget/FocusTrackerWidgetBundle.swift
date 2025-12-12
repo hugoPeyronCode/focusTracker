2d9f97336bcaa786de5383ecf856ec091abda21c
hugoPeyronCode <hugopeyron@gmail.com>
2025-12-12 15:03:42 +0100

    Live activity Added

---
+++ FocusTrackerWidget/FocusTrackerWidgetBundle.swift
@@ -0,0 +1,18 @@
+//
+//  FocusTrackerWidgetBundle.swift
+//  FocusTrackerWidget
+//
+//  Created by Hugo Peyron on 12/12/2025.
+//
+
+import WidgetKit
+import SwiftUI
+
+@main
+struct FocusTrackerWidgetBundle: WidgetBundle {
+    var body: some Widget {
+        FocusTrackerWidget()
+        FocusTrackerWidgetControl()
+        FocusTrackerWidgetLiveActivity()
+    }
+}
