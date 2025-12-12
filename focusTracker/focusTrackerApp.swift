dc8bb4461af6b08b37096f6bf1f9f7fe5acd960c
hugoPeyronCode <hugopeyron@gmail.com>
2025-12-12 11:59:47 +0100

    Initial Commit

---
+++ focusTracker/focusTrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  focusTrackerApp.swift
+//  focusTracker
+//
+//  Created by Hugo Peyron on 12/12/2025.
+//
+
+import SwiftUI
+
+@main
+struct focusTrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
